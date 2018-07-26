8a4828c73ba967b6cd046580e2358ea169ea122f
mpakus <mrak69@gmail.com>
2018-07-26 16:37:24 +0700

    Fixed ambiguous id bug;

--- lib/uniqable.rb
+++ lib/uniqable.rb
@@ -34,7 +34,7 @@ module Uniqable
     def where_uniqable(uid)
       where_sql = key_uid?(uid) ?
                       uniqable_fields.map { |r| "#{table_name}.#{r} = :uid" }.join(' OR ') :
-                      "#{self.primary_key} = :uid"
+                      "#{table_name}.#{self.primary_key} = :uid"
       where(where_sql, uid: uid)
     end
 
